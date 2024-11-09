d00dcae925f60bd712893c3e404b3b37af6c5b09
Andrei Ursachi <strife@Andreis-Laptop.local>
2024-11-09 14:25:42 +0100

    blog project ex 13-14 part 4

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,6 +9,21 @@ blogsRouter.get('/', (request, response) => {
     })
 })
 
+blogsRouter.get('/:id', (request, response, next) => {
+  const id = request.params.id
+
+  Blog
+    .findById(id)
+    .then(blog => {
+      if (blog === null) {
+        response.status(404).json({error: "Not Found"})
+      } else {
+        response.status(200).json(blog)
+      }
+    })
+    .catch(error => next(error))
+})
+
 blogsRouter.post('/', (request, response, next) => {
   const blog = new Blog(request.body)
 
@@ -19,5 +34,37 @@ blogsRouter.post('/', (request, response, next) => {
     })
     .catch(error => next(error));
 })
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const newBody = request.body
+  const id = request.params.id
+
+  Blog
+    .findByIdAndUpdate(id, newBody, { new: true, runValidators: true })
+    .then(result => {
+      if (result) {
+        response.status(204).end();
+      } else {
+        response.status(404).json({ error: 'Blog not found' });
+      }
+    })
+    .catch(error => next(error));
+
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  const id = request.params.id;
+
+  Blog
+    .findByIdAndDelete(id)
+    .then(result => {
+      if (result) {
+        response.status(204).end();
+      } else {
+        response.status(404).json({ error: 'Blog not found' });
+      }
+    })
+    .catch(error => next(error));
+});
 
 module.exports = blogsRouter
--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -37,7 +37,7 @@ test('blogs contain id as unique identifier', async () => {
   }
 })
 
-test('blogs post works as expected', async () => {
+test.only('blogs post works as expected', async () => {
   testInsert = {
     title: "Deep Learning",
     author: "Ian Goodfellow",
@@ -116,6 +116,88 @@ test('backend returns 400 if no url of no title', async () => {
     .post('/api/blogs')
     .send(testInsert[2])
     .expect(400)
+})
+
+test('check for deletion of blog', async () => {
+  const testInsert = {
+    title: "Algorithms Unlocked",
+    author: "Thomas H. Cormen",
+    url: "https://example.com/algorithms-unlocked",
+    likes: 14
+  }
+
+  let id = null
+  function extractId(res) {
+    for (r of res._body){
+      if (r.title === testInsert.title && r.author === testInsert.author && r.url === testInsert.url){
+        id = r.id
+        return true
+      }
+    }
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(testInsert)
+    .expect(201)
+
+  await api
+    .get('/api/blogs')
+    .expect(extractId)
+
+  await api
+    .delete(`/api/blogs/${id}`)
+    .expect(204)
+
+  await api
+    .get(`/api/blogs/${id}`)
+    .expect(404)
+})
+
+test.only('check for update of nr or likes on a blog', async () => {
+  testInsert = {
+    title: "Programming Pearls",
+    author: "Jon Bentley",
+    url: "https://example.com/programming-pearls",
+    likes: 22
+  }
+  const new_nr_of_likes = { likes: 23 }
+
+  let id = null
+  function extractId(res) {
+    for (r of res._body){
+      if (r.title === testInsert.title && r.author === testInsert.author && r.url === testInsert.url){
+        id = r.id
+        return true
+      }
+    }
+    throw new Error(`id is not in db`)
+  }
+
+  function checkNrLikes(res) {
+    if (res !== null){
+      if (!(res._body.likes === new_nr_of_likes.likes)){
+        throw new Error(`the number of likes is ${res._body.likes} and it should be ${new_nr_of_likes.likes}`)
+      } else {
+        return true
+      }
+    }
+    throw new Error('the id is not in the database')
+  }
+
+  await api
+    .get('/api/blogs')
+    .expect(extractId)
+
+  await api
+    .put(`/api/blogs/${id}`)
+    .send(new_nr_of_likes)
+    .set('Accept', 'application/json')
+    .expect(204)
+
+  await api
+    .get(`/api/blogs/${id}`)
+    .expect(checkNrLikes)
 })
 
 after(async () => {
